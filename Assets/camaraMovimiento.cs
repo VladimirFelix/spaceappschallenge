0d59c055199b31fbd1055c7992fcd9ec9d8963b3
idoontcare <idoontcare@hotmail.com>
2014-04-12 14:35:02 -0600

    Nuevos items

---
+++ Assets/camaraMovimiento.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camaraMovimiento : MonoBehaviour {
+
+	Transform player;
+	// Use this for initialization
+	void Start () {
+		GameObject player_go = GameObject.FindGameObjectsWithTag ("Player");
+
+		if (player_go == null) {
+			Debug.LogError("No se pudo encontrar jugador!");
+			return;
+		}
+		player = player_go.transform;		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
